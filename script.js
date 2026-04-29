9a7903f09a204b54e4440dbf2f7173bf44c93c74
Zoe McGee <zoe.c.mcgee@gmail.com>
2026-04-29 08:46:30 -0500

    simplify the code to better suit he setInterval function

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ var clockApp = {
     formatNumber: function (num) {
         // TODO: If num is less than 10, return it with a "0" in front
         // Otherwise, just return the num
-        // https://www.w3schools.com/jsref/jsref_gethours.asp
+
         if (num < 10) {
             return "0" + num;
         }
         return num;
+        // https://www.w3schools.com/jsref/jsref_gethours.asp
     },
 
     getTimeString: function () {
@@ -27,58 +28,41 @@ var clockApp = {
         m = this.formatNumber(m);
         s = this.formatNumber(s);
 
+        if (!is24HourFormat) {
+            // credit to: https://stackoverflow.com/questions/10556879/changing-the-1-24-hour-to-1-12-hour-for-the-gethours-method
+            h = now.getHours() % 12 || 12;
+            return h + ":" + m + ":" + s;
+        }
+
+
+
         return h + ":" + m + ":" + s;
     },
 
     updateDisplay: function () {
         // TODO: Get the element 'clock-display' and change its text
         // to the result of this.getTimeString()
+
         const clock = document.getElementById("clock-display");
 
         clock.innerHTML = this.getTimeString();
-    },
-    tweleveHourDisplay: function () {
-        var now12 = new Date();
-        var h12 = now12.getHours();
-        var m12 = now12.getMinutes();
-        var s12 = now12.getSeconds();
-
-        m12 = this.formatNumber(m12);
-        s12 = this.formatNumber(s12);
-
-        // credit to: https://stackoverflow.com/questions/10556879/changing-the-1-24-hour-to-1-12-hour-for-the-gethours-method
-        if (h12 > 12) {
-            h12 = now12.getHours() % 12 || 12;
-            return h12 + ":" + m12 + ":" + s12;
-        }
     },
 };
 
 // 3. The Standalone Functions
 function handleToggle() {
     // TODO: Flip the is24HourFormat variable (true to false, or false to true)
-    is24HourFormat = false;
+    is24HourFormat = !is24HourFormat;
 }
 
 // 4. Starting the Clock
 // TODO: Use setInterval to call clockApp.updateDisplay every 1000ms
 // setInterval(function, ms)
 
-clockApp.updateDisplay(setInterval(handleToggle(), 1000));
+setInterval(() => {
+    clockApp.updateDisplay();
+}, 1000);
 
-// 2. Add the Event Listener
-toggleBtn.addEventListener("click", () => {
-    // 3. Use the .toggle() method on classList
-    // This adds 'dark-theme' if it's missing, and removes it if it's there
-    document.body.classList.toggle("dark-theme");
-
-    // Bonus: Update the button text dynamically
-    if (document.body.classList.contains("dark-theme")) {
-        toggleBtn.textContent = "Switch to Light Mode";
-    } else {
-        toggleBtn.textContent = "Switch to Dark Mode";
-    }
-});
 
 const twelveHour = document.querySelector("#clock-button");
 
@@ -86,13 +70,23 @@ const display = document.getElementById("clock-display");
 
 twelveHour.addEventListener("click", function () {
     document.body.classList.toggle("clock-button");
-      if (document.body.classList.contains("clock-button")) {
+    if (document.body.classList.contains("clock-button")) {
         is24HourFormat = false;
-        display.innerHTML = clockApp.tweleveHourDisplay();
+        clockApp.updateDisplay();
         twelveHour.textContent = "Click to 24 Hour Time";
-      } else {
+    } else {
         is24HourFormat = true;
-        display.innerHTML = clockApp.getTimeString();
+        clockApp.updateDisplay();
         twelveHour.textContent = "Click to 12 Hour Time";
-      }
+    }
+});
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        toggleBtn.textContent = "Switch to Light Mode";
+    } else {
+        toggleBtn.textContent = "Switch to Dark Mode";
+    }
 });
